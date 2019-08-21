5af4995df852dc7d99b06e7e42822ad16ac21c09
ZhangYuanSheng <2862297923@qq.com>
2019-08-22 00:27:42 +0800

    base

---
+++ src/main/java/com/example/jenkins/HelloController.java
@@ -0,0 +1,29 @@
+/*
+  Copyright (C), 2018-2019, ZhangYuanSheng
+  FileName: HelloController
+  Author:   ZYS
+  Date:     2019/8/22 00:25
+  Description: 
+  History:
+  <author>          <time>          <version>          <desc>
+  作者姓名            修改时间           版本号              描述
+ */
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * description: 无
+ *
+ * @author ZYS
+ * @version 1.0
+ */
+@RestController
+public class HelloController {
+    
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
