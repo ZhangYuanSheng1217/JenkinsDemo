85a073c2254805fde9eb8924ee64dd52b8a82a08
ZhangYuanSheng <2862297923@qq.com>
2019-08-26 00:04:49 +0800

    stop.sh fix bug(can't kill proc)

--- stop.sh
+++ stop.sh
@@ -3,16 +3,15 @@
 # 将应用停止
 #stop.sh
 
-if [ ! -n "$1" ] ;then
-    echo "[ERROR] >>> 未检测到参数[\$1]，请重试！"
+if [ ! -n "$1" ]; then
+    echo "[ERROR] >>> 未检测到参数[JAR_NAME]，请重试！"
 else
-    NAME=$1
-    echo "正在停止SpringBoot项目: $NAME"
-    pid=`ps -ef | grep "$NAME" | grep -v grep | awk '{print $2}'`
-    if [ -n "$pid" ]
-    then
+    JAR_NAME=$1
+    echo "正在停止SpringBoot项目: $JAR_NAME"
+    pid=`ps -ef | grep "$JAR_NAME" | grep -v grep | awk '{print $2}'`
+    if [ -n "$pid" ]; then
        echo "SpringBoot项目进程PID: $pid"
-       kill -9 $pid
+       sudo kill -9 $pid
     fi
     echo 'SpringBoot项目进程已杀死！'
 fi
