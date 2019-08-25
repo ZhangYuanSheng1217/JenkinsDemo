56acb11d865a40722a30192533fa4147b3a9167e
ZhangYuanSheng <2862297923@qq.com>
2019-08-25 23:12:35 +0800

    add controller: home fix

--- src/main/java/com/example/jenkins/HelloController.java
+++ src/main/java/com/example/jenkins/HelloController.java
@@ -38,7 +38,7 @@ public class HelloController {
     
     @GetMapping("home")
     public String home() {
-        return "Jenkins自动化CI测试项目! Welcome Home!!!";
+        return "Jenkins自动化CI测试项目! Welcome Home'Page!";
     }
     
     @RequestMapping("other")
